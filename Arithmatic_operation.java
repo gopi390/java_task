30c2a64587e036ed76ab034e7a12a52b8a7b568a
gopi390 <gopinath200513@gmail.com>
2025-09-14 20:56:43 +0530

    Add files via upload

---
+++ Arithmatic_operation.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Arithmatic_operation
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first value:");
+        a = scan.nextInt();
+        System.out.println("Enter the Secound value:");
+        b= scan.nextInt();
+        System.out.println("the a+b:"+(a+b));
+        System.out.println("the a-b:"+(a-b));
+        System.out.println("the a+b:"+(a+b));
+        System.out.println("the a*b:"+a*b);
+        System.out.println("the a/b:"+a/b);
+        System.out.println("the a%b:"+a%b);
+        System.out.println("addfition assginment operator");
+        System.out.println("the a+=6:"+(a+=6));
+        System.out.println("the a++:"+a++);
+        System.out.println("the a--:"+a--);
+        System.out.println("the ++b:"+(--b));
+        System.out.println("the ++b:"+(++b));
+        System.out.println("the a:"+a);
+        System.out.println("the b:"+b);
+
+
+    }
+
+}
